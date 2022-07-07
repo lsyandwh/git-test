3f67b53d522b4f43a11623a8e05bec614c725710
lshuyun <2352630787@qq.com>
2022-07-07 09:52:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart:
+ * @crecte 2022-07-07-9:48
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
